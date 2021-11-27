2071cf95a7312200a48df69a1ad94ed11749aaee
alejandrogsk <alejandrogsk9900@gmail.com>
2021-11-27 12:07:22 -0300

    adding images

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,9 +3,6 @@ import React from 'react'
 const Footer = () => {
     return (
         <footer className="footer" >
-
-
-
             <p>Copyright © Alejandro Suarez | 2021</p>
         </footer>
     )
